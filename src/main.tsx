ed52a4c587322dca889d5af7c49dbe3fe03a6398
HSCHELO <knr0013@gmail.com>
2024-06-01 01:09:21 +0900

    chore:필요없는 파일 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import background from "../src/assets/background.gif";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div
+      className="w-screen h-screen bg-cover bg-no-repeat bg-center opacity-80"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <App />
+    </div>
+  </React.StrictMode>
+);
